f173d39329356132cb7b8b54ab7bb25dd4084527
yinft <yinft@zencloud.net.cn>
2018-12-12 22:18:11 +0800

    初始化

---
+++ src/main/java/com/lemon/LemonApplication.java
@@ -0,0 +1,14 @@
+package com.lemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lemon/LemonApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lemon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LemonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

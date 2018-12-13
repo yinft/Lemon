2ed54c988d4e3b21c2934712a471ac749b98fc51
yinft <yinft@zencloud.net.cn>
2018-12-13 17:30:32 +0800

    更新pom

--- src/main/java/com/lemon/LemonApplication.java
+++ src/main/java/com/lemon/LemonApplication.java
@@ -3,6 +3,7 @@ package com.lemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LemonApplication {
 

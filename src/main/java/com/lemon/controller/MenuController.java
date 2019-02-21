7fb13094959b311896fbb3950527e7b0f4dd2024
yinft <yinft@zencloud.net.cn>
2019-02-21 17:00:03 +0800

    添加新增，删除菜单的Controller方法

--- src/main/java/com/lemon/controller/MenuController.java
+++ src/main/java/com/lemon/controller/MenuController.java
@@ -1,13 +1,10 @@
 package com.lemon.controller;
 
-import com.lemon.config.auth.JwtUser;
 import com.lemon.domain.dto.MenuDto;
-import com.lemon.domain.entity.Role;
 import com.lemon.domain.entity.User;
 import com.lemon.domain.vo.MenuVo;
 import com.lemon.domain.vo.ResultMap;
 import com.lemon.service.MenuService;
-import com.lemon.service.PermissionService;
 import com.lemon.service.UserService;
 import com.lemon.utils.JwtTokenUtil;
 import io.swagger.annotations.Api;
@@ -15,18 +12,14 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Set;
+
 
 
 /**
@@ -57,15 +50,11 @@ public class MenuController {
     @ApiOperation(value = "构建组件路由菜单", notes = "构建组件路由菜单", consumes = "application/json")
     @GetMapping(value = "/menus/build")
     public ResultMap<List<MenuVo>> buildMenus(HttpServletRequest request){
-
         User user = userService.findByName(jwtTokenUtil.getUserName(request));
-
         List<MenuDto> menuDTOList = menuService.getMenusByUserId(user.getId());
         List<MenuVo> menuVoList=menuService.buildMenus((List<MenuDto>)menuService.buildTree(menuDTOList).get("content"));
-
         return ResultMap.success(menuVoList);
     }
-
 
     /**
      * 返回全部的菜单
@@ -81,14 +70,40 @@ public class MenuController {
     }
 
 
-//    @ApiImplicitParam(paramType = "header", name = "Authorization", defaultValue = "Bearer ")
-//    @ApiOperation(value = "查询菜单", notes = "查询菜单", consumes = "application/json")
-//    @GetMapping(value = "/menus")
-//    @PreAuthorize("hasAnyRole('ADMIN','MENU_ALL','MENU_SELECT')")
-//    public ResultMap<List> getMenuTree(){
-//        List  list=menuService.getMenuTree(menuService.getByPid(0L));
-//        return ResultMap.success(list);
-//    }
 
+    @ApiImplicitParam(paramType = "header", name = "Authorization", defaultValue = "Bearer ")
+    @ApiOperation(value = "查询菜单", notes = "查询菜单", consumes = "application/json")
+    @GetMapping(value = "/menus")
+    @PreAuthorize("hasAnyRole('ADMIN','MENU_ALL','MENU_SELECT')")
+    public ResultMap<List> getMenus(){
+        return null;
+    }
+
+
+    @ApiImplicitParam(paramType = "header", name = "Authorization", defaultValue = "Bearer ")
+    @ApiOperation(value = "新增菜单", notes = "新增菜单", consumes = "application/json")
+    @GetMapping(value = "/menus")
+    @PreAuthorize("hasAnyRole('ADMIN','MENU_ALL','MENU_CREATE')")
+    public ResultMap<List> creatMenus(){
+       return null;
+    }
+
+
+    @ApiImplicitParam(paramType = "header", name = "Authorization", defaultValue = "Bearer ")
+    @ApiOperation(value = "修改菜单", notes = "修改菜单", consumes = "application/json")
+    @GetMapping(value = "/menus")
+    @PreAuthorize("hasAnyRole('ADMIN','MENU_ALL','MENU_EDIT')")
+    public ResultMap<List> updateMenus(){
+        return null;
+    }
+
+
+    @ApiImplicitParam(paramType = "header", name = "Authorization", defaultValue = "Bearer ")
+    @ApiOperation(value = "删除菜单", notes = "删除菜单", consumes = "application/json")
+    @GetMapping(value = "/menus")
+    @PreAuthorize("hasAnyRole('ADMIN','MENU_ALL','MENU_DELETE')")
+    public ResultMap<List> deleteMenus(){
+        return null;
+    }
 
 }
